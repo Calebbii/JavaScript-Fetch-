a02d449e17fd0e890f08895f0183c319ba3fb208
Calebbii <biicaleb316@gmail.com>
2026-04-09 05:51:33 +0300

    JavaScript Fetch

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 console.log("It works")
 
-const baseUrl = "http://localhost:3000/characters"
+const base_url = "http://localhost:3000/characters"
 
-// console.log(baseUrl)
 
 // Fetch
-fetch(baseUrl)
+fetch(base_url)
     .then(res => res.json())
     .then(data => {
         displayContent(data)
     })
 
 
-// Assignment in the next 10 mintues Create a function called displayContent that is going to display our data to the browser
+// Function Syntax
+// function nameOfFunc () {
+// }
 
-// Function syntax
 function displayContent(data) {
     const dataContainer = document.getElementById('data') // Finds an HTML element with id="data"
     dataContainer.innerHTML = '';
